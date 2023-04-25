1463fc0f3131a8308849ef59bb6fcfc561fa172b
Satan Sharma <satansharma7549@gmail.com>
2023-04-25 23:40:05 +0530

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,43 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config/config.env" });
+const ConnectDB = require("./config/db.connect");
+
+/*App connect */
+const port = process.env.PORT || 8090;
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
+});
+
+app.listen(port, async () => {
+  await ConnectDB();
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhabled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+}); 
+/* 
+//useing basic ("http") to create server; 
+const http = require("http");
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.end("Hello");
+  } else if (request.url === "/reports") {
+    response.end("Here are the reports");
+  } else if (request.url === "/data") {
+    response.end("Data....");
+  }
+});
+server.listen(4500, () => {
+  console.log("Listening on the port 4500");
+});
+
+*/
