c26a0c576524e63adfac846a4cd43ec344639e34
Satan Sharma <satansharma7549@gmail.com>
2023-04-27 01:09:23 +0530

    Update error.js

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -4,6 +4,13 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  // wrong product id error
+  if (err.name == "CastError") {
+    const message = `Resource not found. Inavlid: ${err.path}`;
+    err = ErrorHandler(message, 400);
+  }
+
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
