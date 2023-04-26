b179153603fa7ac2ac568ad86bf64f503fc2d194
Satan Sharma <satansharma7549@gmail.com>
2023-04-27 01:09:20 +0530

    Update product.route.js

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
+  deleteProduct,
+  singleProduct,
 } = require("../controllers/product.controller");
 
 const productRoute = express.Router();
 
 productRoute.get("/", getAllProducts);
+productRoute.get("/:id", singleProduct);
 productRoute.post("/create", createProduct);
+productRoute.put("/update/:id", updateProduct);
+productRoute.delete("/delete/:id", deleteProduct);
 
 module.exports = productRoute;
