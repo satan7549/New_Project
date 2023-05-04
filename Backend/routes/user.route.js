ea4f82178a235fdb795a5d6f3f39853d9b9dfb3c
Satan Sharma <satansharma7549@gmail.com>
2023-05-04 23:28:41 +0530

    Update user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { getAllUsers } = require("../controllers/user.controller");
+const { registerUser,userLogin, getAllUsers } = require("../controllers/user.controller");
 const userRoutes = express.Router();
 
+userRoutes.post("/register", registerUser);
+userRoutes.post("/login", userLogin);
 userRoutes.get("/", getAllUsers);
 
 module.exports = userRoutes;
