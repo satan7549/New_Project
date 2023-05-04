b292ad2f084d08a691c791e4deeeec3a949bc5c6
Satan Sharma <satansharma7549@gmail.com>
2023-05-05 02:45:37 +0530

    import login and logout function

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser,userLogin, getAllUsers } = require("../controllers/user.controller");
+const {
+  registerUser,
+  userLogin,
+  logoutUser,
+  getAllUsers,
+} = require("../controllers/user.controller");
 const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUser);
 userRoutes.post("/login", userLogin);
+userRoutes.get("/logout", logoutUser);
 userRoutes.get("/", getAllUsers);
 
 module.exports = userRoutes;
