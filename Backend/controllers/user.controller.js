37f8abdf318c70cecf9f8d48e6bbdaab2f6e6063
Satan Sharma <satansharma7549@gmail.com>
2023-05-04 23:28:23 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,45 @@
-const UserModel = require("../models/userModels");
+const catchAsyncErrors = require("../middleware/catchAsyncError");
+const ErrorHandler = require("../utils/errorHandler");
+const userModel = require("../models/userModels");
+const bcrypt = require("bcrypt");
+
+// register User;
+
+const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  // // Check if user already exists
+  // const userExists = await userModel.findOne({ email });
+  // if (userExists) {
+  //   // return next(new ErrorHandler("User already exists", 400));
+  //   return res
+  //     .status(400)
+  //     .json({ success: true, message: "User already exists" });
+  // }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  const user = new userModel({
+    name,
+    email,
+    password: hashedPassword,
+    avatar: {
+      public_id: "Tempory Avatar public ID",
+      url: "temprory Avatar url ",
+    },
+  });
+
+  const new_User = await user.save();
+  res.status(201).json({ success: true, new_User });
+});
+
+const userLogin = catchAsyncErrors(async (req, res) => {
+  const { email, password } = req.body;
+  const userExists = await userModel.findOne({ email });
+
+ 
+});
 
 const getAllUsers = async (req, res) => {
   try {
@@ -9,5 +50,7 @@ const getAllUsers = async (req, res) => {
 };
 
 module.exports = {
+  registerUser,
+  userLogin,
   getAllUsers,
 };
