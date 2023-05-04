62b4b84a95be3f1b62b5b0490587bb72f6676619
Satan Sharma <satansharma7549@gmail.com>
2023-05-04 23:27:42 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,6 +18,7 @@ app.get("/", (req, res) => {
 /* import all routes */
 const user = require("./routes/user.route");
 const product = require("./routes/product.route");
+
 app.use("/user", user);
 app.use("/product", product);
 
