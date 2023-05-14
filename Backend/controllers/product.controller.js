a5ac1f86297c245b3aaebeeb68f3eb6f332dcc95
Satan Sharma <satansharma7549@gmail.com>
2023-05-14 11:14:35 +0530

    Update product.controller.js

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -5,6 +5,7 @@ const { search, filter, sort, paginate } = require("../utils/apiFeatures");
 
 // Create Product -- Admin
 const createProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id;
   const product = new productModel(req.body);
   const new_product = await product.save();
   res.status(201).json({ success: true, new_product });
@@ -14,7 +15,7 @@ const createProduct = catchAsyncErrors(async (req, res, next) => {
 const getAllProducts = catchAsyncErrors(async (req, res, next) => {
   // Get the query string parameters from the request object
   let resultPerPage = 1;
-  
+
   let productCount = await productModel.countDocuments();
   // Create a base query to get all products
   // const produts = await productModel.find();
@@ -35,7 +36,6 @@ const getAllProducts = catchAsyncErrors(async (req, res, next) => {
   // Execute the query and send the response
   const products = await query;
 
-  
   res.status(201).json({ success: true, products, productCount });
 });
 
@@ -44,7 +44,7 @@ const singleProduct = catchAsyncErrors(async (req, res, next) => {
   const productID = req.params.id;
   const product = await productModel.findById(productID);
   if (!product) {
-    return next(new ErrorHandler("Product Not Found", 500));
+    return next(ErrorHandler("Product Not Found", 500));
     // res.status(500).json({ success: false, message: "Product Not Found" });
   }
   const produts = await productModel.findById(productID);
@@ -60,7 +60,7 @@ const updateProduct = catchAsyncErrors(async (req, res) => {
   const productID = req.params.id;
   const product = await productModel.findById(productID);
   if (!product) {
-    return next(new ErrorHandler("Product Not Found", 500));
+    return next(ErrorHandler("Product Not Found", 500));
     // res.status(500).json({ success: false, message: "Product Not Found" });
   }
   const produts = await productModel.findByIdAndUpdate(productID, req.body, {
@@ -79,7 +79,7 @@ const deleteProduct = catchAsyncErrors(async (req, res) => {
   const productID = req.params.id;
   const product = await productModel.findById(productID);
   if (!product) {
-    return next(new ErrorHandler("Product Not Found", 500));
+    return next(ErrorHandler("Product Not Found", 500));
     // res.status(500).json({ success: false, message: "Product Not Found" });
   }
   await productModel.findByIdAndDelete(productID);
