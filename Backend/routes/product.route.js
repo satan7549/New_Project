7484327c0f2ff6def6a88fbab5f215dbb1a1b5be
Satan Sharma <satansharma7549@gmail.com>
2023-05-14 11:15:16 +0530

    Update product.route.js

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -6,14 +6,20 @@ const {
   deleteProduct,
   singleProduct,
 } = require("../controllers/product.controller");
-const {isUserAuthenticated,checkRole} = require("../middleware/userAuth");
+const { isUserAuthenticated, checkRole } = require("../middleware/userAuth");
 
 const productRoute = express.Router();
 
-productRoute.get("/", isUserAuthenticated,checkRole("admin"), getAllProducts);
+productRoute.get("/", getAllProducts);
 productRoute.get("/:id", singleProduct);
-productRoute.post("/create", isUserAuthenticated, createProduct);
-productRoute.put("/update/:id", isUserAuthenticated, updateProduct);
-productRoute.delete("/delete/:id", isUserAuthenticated, deleteProduct);
+/* Admin Access  only below routes */
+productRoute.post("/create", isUserAuthenticated, checkRole, createProduct);
+productRoute.put("/update/:id", isUserAuthenticated, checkRole, updateProduct);
+productRoute.delete(
+  "/delete/:id",
+  isUserAuthenticated,
+  checkRole,
+  deleteProduct
+);
 
 module.exports = productRoute;
